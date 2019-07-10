999880247bb26370beb1d6a5ed64397aa159120d
MacBook Air <hello@MacBooks-MacBook-Air-8.local>
2019-07-10 15:00:45 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:tour_app/app.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return App();
+  }
+}
