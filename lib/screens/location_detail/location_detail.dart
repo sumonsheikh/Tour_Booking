bd46b05339ddeba4bce2334ba5f9a22ca3f7ac14
MacBook Air <hello@MacBooks-MacBook-Air-8.local>
2019-07-10 16:12:09 +0600

    Add Banner

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'text_section.dart';
+import 'image_banner.dart';
 class LocationDetail extends StatelessWidget{
   @override
   Widget build(BuildContext context){
@@ -12,6 +13,7 @@ class LocationDetail extends StatelessWidget{
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          ImageBanner("assets/images/kiyomizu-dera.jpg"),
           TextSection(Colors.red),
           TextSection(Colors.green),
           TextSection(Colors.blue),
